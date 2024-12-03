b1c8b30c4896dcc6d25d127a14ca954a81204eee
CMAXMAPEN <anthonys4.calderon@gmail.com>
2024-12-02 22:25:31 -0500

    made car table

--- Lab_5/src/main/java/com/example/lab_5/DBUtil.java
+++ Lab_5/src/main/java/com/example/lab_5/DBUtil.java
@@ -9,6 +9,7 @@ public class DBUtil {
     private static Connection connection = null;
     private static Statement statement = null;
 
+    //dbConnect and disconnect
     public static void dbConnect()
     {
         String dbURL = "jdbc:oracle:thin:@199.212.26.208:1521:SQLD";
@@ -34,7 +35,8 @@ public class DBUtil {
         }
     }
 
-    public static void createTable() throws SQLException {
+    //create and drop tables
+    public static void createOwnerTable() throws SQLException {
         dbConnect();
         String sql = "CREATE TABLE Owner(ownerID INTEGER NOT NULL PRIMARY KEY, name VARCHAR(100) NOT NULL, " +
                 "address VARCHAR(200) NOT NULL, phone VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL)";
@@ -44,7 +46,7 @@ public class DBUtil {
         dbDisconnect();
     }
 
-    public static void dropTable() throws SQLException
+    public static void dropOwnerTable() throws SQLException
     {
         dbConnect();
         String sql = "DROP TABLE Owner";
@@ -54,7 +56,32 @@ public class DBUtil {
         dbDisconnect();
     }
 
-    public static void insertData(int ownerID, String name, String address, String phone, String email) throws SQLException{
+    public static void createCarTable() throws SQLException {
+        dbConnect();
+        System.out.println("create car");
+        String sql = "CREATE TABLE Car(carID INTEGER NOT NULL PRIMARY KEY, make VARCHAR(100) NOT NULL, " +
+                "model VARCHAR(100) NOT NULL, VIN INTEGER NOT NULL, buildYear INTEGER NOT NULL, type VARCHAR(100) NOT NULL)";
+        statement.execute(sql);
+        System.out.println("Car is created!");
+        if (statement != null) statement.close();
+        dbDisconnect();
+    }
+
+    public static void dropCarTable() throws SQLException
+    {
+        System.out.println("drop car");
+        dbConnect();
+        String sql = "DROP TABLE Car";
+        statement.execute(sql);
+        System.out.println("Car is dropped!");
+        if(statement != null) statement.close();
+        dbDisconnect();
+    }
+
+
+
+
+    public static void insertOwnerData(int ownerID, String name, String address, String phone, String email) throws SQLException{
         dbConnect();
         String sql = "INSERT INTO Owner VALUES('" + ownerID + "','" + name+ "','" + address + "','" + phone + "','" + email + "')";
         statement.execute(sql);
@@ -63,23 +90,73 @@ public class DBUtil {
         dbDisconnect();
     }
 
-    public static void deleteData(int id) throws SQLException{
+    public static void deleteOwnerData(int id) throws SQLException{
         dbConnect();
-        String sql = "DELETE FROM MyTable WHERE s_id = " + id;
+        //sql can be changed to delete other table values
+        //additionally, we can have a table that automatically changes the "Owner" in sql
+        String sql = "DELETE FROM Owner WHERE ownerID = " + id;
         statement.execute(sql);
         System.out.println("Data is deleted from the table!");
         if (statement != null) statement.close();
         dbDisconnect();
     }
 
-    public static void simpleQuery() throws SQLException {
+    public static void updateOwnerData(int ownerID, String name, String address, String phone,
+                                  String email) throws SQLException
+    {
         dbConnect();
-        String sql = "SELECT * FROM Owner";
+        String sql = "UPDATE Owner SET name = '" + name + "', address = '" + address + "', phone = '"
+            + phone + "', email = '" + email + "' where ownerID = " + ownerID;
+        statement.execute(sql);
+        System.out.println("Data is updated!");
+        if (statement != null) statement.close();
+        dbDisconnect();
+    }
+
+    public static void insertCarData(int carID, String make, String model, int VIN,
+                                     int buildYear, String type) throws SQLException{
+        System.out.println("here");
+        dbConnect();
+        String sql = "INSERT INTO Car VALUES('" + carID + "','" + make+ "','" + model + "','" + VIN + "','" + buildYear + "','"+
+        type + "')";
+        statement.execute(sql);
+        System.out.println("Data is added to the table!");
+        if (statement != null) statement.close();
+        dbDisconnect();
+    }
+
+    public static void deleteCarData(int id) throws SQLException{
+        dbConnect();
+        String sql = "DELETE FROM Car WHERE carID = " + id;
+        statement.execute(sql);
+        System.out.println("Data is deleted from the table!");
+        if (statement != null) statement.close();
+        dbDisconnect();
+    }
+
+    public static void updateCarData(int carID, String make, String model, int VIN,
+                                       int buildYear, String type) throws SQLException
+    {
+        dbConnect();
+        String sql = "UPDATE Car SET make = '" + make + "', model = '" + model + "', VIN = "
+                + VIN + ", buildYear = " + buildYear + ", type = '"+type+"' where carID = " + carID;
+        statement.execute(sql);
+        System.out.println("Data is updated!");
+        if (statement != null) statement.close();
+        dbDisconnect();
+    }
+
+
+
+    public static void simpleQuery() throws SQLException {
+        //can change the sql depending on the query
+        dbConnect();
+        String sql = "SELECT * FROM Car";
         ResultSet resultSet = statement.executeQuery(sql);
         while (resultSet.next()){
-            String s_name = resultSet.getString("name");
-            int s_id = resultSet.getInt("ownerID");
-            System.out.println(s_id + " , " + s_name);
+            String s_make = resultSet.getString("make");
+            int s_id = resultSet.getInt("carID");
+            System.out.println(s_id + " , " + s_make);
         }
         if (statement != null) statement.close();
         dbDisconnect();
@@ -87,6 +164,7 @@ public class DBUtil {
 
     public static ResultSet query(String sql) throws SQLException
     {
+        dbConnect();
         //this is for javafx
         CachedRowSet crs = RowSetProvider.newFactory().createCachedRowSet();
         ResultSet resultSet = statement.executeQuery(sql);
@@ -100,13 +178,18 @@ public class DBUtil {
     //test the DBUtil Methods
     public static void main(String[] args) throws SQLException
     {
-//        DBUtil.dbConnect();
-//        DBUtil.dbDisconnect();
-        DBUtil.dropTable();
-        DBUtil.createTable();
-        DBUtil.insertData(123123123,"Anthony", "Street", "123-123","email.com");
-        DBUtil.simpleQuery();
-//        DBUtil.deleteData(100);
+////        DBUtil.dbConnect();
+////        DBUtil.dbDisconnect();
+//        DBUtil.dropOwnerTable();
+//        DBUtil.createOwnerTable();
+////        DBUtil.insertData(123123123,"Anthony", "Street", "123-123","email.com");
+////        DBUtil.simpleQuery();
+////        DBUtil.deleteData(123123123);
+////        DBUtil.simpleQuery();
+
+//        DBUtil.insertCarData(12312312, "Hond", "Civic", 456, 2020, "Good");
+//        DBUtil.simpleQuery();
+//        DBUtil.deleteCarData(12312312);
 //        DBUtil.simpleQuery();
 
     }
--- Lab_5/src/main/java/com/example/lab_5/MyController.java
+++ Lab_5/src/main/java/com/example/lab_5/MyController.java
@@ -1,5 +1,6 @@
 package com.example.lab_5;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,18 +10,23 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import static java.lang.Integer.parseInt;
 
 public class MyController {
+
+    //owner elements
     @FXML
     private TableView ownerTable;
     @FXML
     private TableColumn ownerCol1;
     @FXML
     private TableColumn ownerCol2;
+
+    //textfields
     @FXML
     private TextField ownerID;
     @FXML
@@ -31,15 +37,19 @@ public class MyController {
     private TextField phone;
     @FXML
     private TextField email;
+
+    //buttons
     @FXML
     private Button ownerInsertButton;
     @FXML
     private Button ownerUpdateButton;
     @FXML
     private Button ownerDeleteButton;
+    @FXML
+    private Button ownerDisplayButton;
 
 
-
+    //repair table
     @FXML
     private TableView repairTable;
     @FXML
@@ -47,6 +57,9 @@ public class MyController {
     @FXML
     private TableColumn repairCol2;
 
+
+    //car table
+    //car elements
     @FXML
     private TableView carTable;
     @FXML
@@ -54,17 +67,130 @@ public class MyController {
     @FXML
     private TableColumn carCol2;
 
+    //textfields
+    @FXML
+    private TextField carID;
+    @FXML
+    private TextField make;
+    @FXML
+    private TextField model;
+    @FXML
+    private TextField VIN;
+    @FXML
+    private TextField buildYear;
+    @FXML
+    private TextField type;
 
 
+
+    //owner table functions
     public void addOwnerData(ActionEvent actionEvent) throws SQLException {
-        DBUtil.insertData(parseInt(ownerID.getText()), name.getText(), address.getText(), phone.getText(),
+        DBUtil.insertOwnerData(parseInt(ownerID.getText()), name.getText(), address.getText(), phone.getText(),
         email.getText());
+        populateCarData();
+    }
+
+    public void ownerDisplay() throws SQLException {
+        DBUtil.simpleQuery();
+        populateOwnerData();
+    }
+
+    public void ownerUpdate() throws SQLException
+    {
+        DBUtil.updateOwnerData(parseInt(ownerID.getText()), name.getText(), address.getText(), phone.getText(),
+                email.getText());
+        populateOwnerData();
+    }
+
+    public void ownerDelete() throws SQLException
+    {
+        DBUtil.deleteOwnerData(parseInt(ownerID.getText()));
+        populateOwnerData();
     }
 
 
-    public void initialize()
-    {
+    public void populateOwnerData() throws SQLException {
+        ResultSet rs = DBUtil.query("SELECT * FROM Owner");
+        // create a list of object that we want to add to the table
+        ObservableList<Owner> owners = FXCollections.observableArrayList();
+        //add object one by one
+        while (rs.next()){
+            Owner owner = new Owner(rs.getInt("ownerID"), rs.getString("name"),
+                    rs.getString("address"), rs.getString("phone"),
+                    rs.getString("email"));
+            owners.add(owner);
+        }
 
+        ownerCol2.setCellValueFactory(new PropertyValueFactory("name"));
+
+        //clear the table
+        ownerTable.getItems().clear();
+        //add all owners
+        ownerTable.getItems().addAll(owners);
+        //sort the table
+        ownerCol2.setSortType(TableColumn.SortType.ASCENDING);
+        ownerTable.sort();
+    }
+
+    //repair table functions
+
+
+
+
+    //car table functions
+    public void addCarData(ActionEvent actionEvent) throws SQLException {
+        DBUtil.insertCarData(parseInt(carID.getText()), make.getText(), model.getText(), parseInt(VIN.getText()),
+                parseInt(buildYear.getText()), type.getText());
+        populateCarData();
+    }
+
+    public void carDisplay() throws SQLException {
+        DBUtil.simpleQuery();
+        populateCarData();
+    }
+
+    public void carUpdate() throws SQLException
+    {
+        DBUtil.updateCarData(parseInt(carID.getText()), make.getText(), model.getText(), parseInt(VIN.getText()),
+                parseInt(buildYear.getText()), type.getText());
+        populateCarData();
+    }
+
+    public void carDelete() throws SQLException
+    {
+        DBUtil.deleteCarData(parseInt(carID.getText()));
+        populateCarData();
+    }
+
+    public void populateCarData() throws SQLException {
+        ResultSet rs = DBUtil.query("SELECT * FROM Car");
+        // create a list of object that we want to add to the table
+        ObservableList<Car> cars = FXCollections.observableArrayList();
+        //add object one by one
+        while (rs.next()){
+            Car car = new Car(rs.getInt("carID"), rs.getString("make"),
+                    rs.getString("model"), rs.getInt("VIN"),
+                    rs.getInt("buildYear"), rs.getString("type"));
+            cars.add(car);
+        }
+
+        carCol2.setCellValueFactory(new PropertyValueFactory("make"));
+
+        //clear the table
+        carTable.getItems().clear();
+        //add all cars
+        carTable.getItems().addAll(cars);
+        //sort the table
+        carCol2.setSortType(TableColumn.SortType.ASCENDING);
+        carTable.sort();
+    }
+
+
+
+
+    public void initialize() throws SQLException {
+        populateOwnerData();
+        populateCarData();
     }
 
 }
